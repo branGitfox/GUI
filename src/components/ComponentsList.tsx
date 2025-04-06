c48c8b8923c2b4050e151c90c3683cb9bdae9c5d
AntoinnetRjuan <antoinnetrajean@gmail.com>
2025-04-06 15:32:46 +0300

    ref:change title of 2th  elements list

--- src/components/ComponentsList.tsx
+++ src/components/ComponentsList.tsx
@@ -54,7 +54,7 @@ const ComponentsList: React.FC = () => {
                                     </svg>
                                 </div>
                                 <div className="flex justify-between">
-                                    <h2 className="text-3xl font-bold">Repositories count Check</h2>
+                                    <h2 className="text-3xl font-bold">Repositories & collabs count Check</h2>
                                     <span className="text-xl">
                                         <button className="btn btn-square btn-ghost">
                                             <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path></g></svg>
