d87eec0f85826baeb9d0195b0ba3a5867dd15d90
BranGitfox <vixfgit@gmail.com>
2025-04-06 20:30:43 +0300

    struct: devide components

---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Hero from "./Hero.tsx";
+
+const Home:React.FC = () => {
+    return (
+        <>
+            <Hero/>
+        </>
+    )
+}
+
+export default Home
