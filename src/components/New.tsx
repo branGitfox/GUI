950c3796672f52319c28187e18db3439192662e0
AntoinnetRjuan <ratsarafarajeanantoinnet@gmail.com>
2025-04-08 23:21:22 +0300

    ref:change style in Header, Hero and New (Responsive)

--- src/components/New.tsx
+++ src/components/New.tsx
@@ -1,23 +1,26 @@
 import React from "react";
 
-
 const New:React.FC = () => {
     return (
         <>
-            <div className="flex flex-wrap gap-10 p-4 justify-evenly mb-5 mt-3 w-full" >
-                <div className="indicator">
-                    <span className="indicator-item badge badge-primary">New</span>
-                    <div className="bg-base-300 grid w-[400px] h-[400px] lg:h-52 lg:w-52 place-items-center">content</div>
+            <div className="flex flex-wrap gap-4 md:gap-6 lg:gap-10 p-3 md:p-4 justify-center mb-5 mt-3 w-full">
+                <div className="relative w-full sm:w-64 md:w-72 lg:w-52">
+                    <span className="absolute -right-2 -top-2 badge badge-primary z-10">New</span>
+                    <div className="bg-base-300 grid h-64 sm:h-64 md:h-72 lg:h-52 place-items-center rounded-lg w-full">
+                        content
+                    </div>
                 </div>
-
-                <div className="indicator">
-                    <span className="indicator-item badge badge-primary">New</span>
-                    <div className="bg-base-300 grid w-[400px] h-[400px] lg:h-52 lg:w-52 place-items-center">content</div>
+                <div className="relative w-full sm:w-64 md:w-72 lg:w-52">
+                    <span className="absolute -right-2 -top-2 badge badge-primary z-10">New</span>
+                    <div className="bg-base-300 grid h-64 sm:h-64 md:h-72 lg:h-52 place-items-center rounded-lg w-full">
+                        content
+                    </div>
                 </div>
-
-                <div className="indicator">
-                    <span className="indicator-item badge badge-primary">New</span>
-                    <div className="bg-base-300 grid w-[400px] h-[400px] lg:h-52 lg:w-52 place-items-center">content</div>
+                <div className="relative w-full sm:w-64 md:w-72 lg:w-52">
+                    <span className="absolute -right-2 -top-2 badge badge-primary z-10">New</span>
+                    <div className="bg-base-300 grid h-64 sm:h-64 md:h-72 lg:h-52 place-items-center rounded-lg w-full">
+                        content
+                    </div>
                 </div>
             </div>
         </>
