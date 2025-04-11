8596bba5353dc5d8d36e5d792457fce4703b08bd
BranGitfox <vixfgit@gmail.com>
2025-04-11 15:02:03 +0300

    prod: fix prod failed deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import Header from './components/Header'
 import {Outlet} from "react-router-dom";
-import getUserInfo, {getReposInfo} from "./api/api.ts";
+
 const App:React.FC = () => {
-    console.log(getReposInfo('BranGitfox', 'GUI'))
     return (
         <>
             <div className='container mx-auto'>
