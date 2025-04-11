1208fd880c99474e924c423ff920168699893942
AntoinnetRjuan <ratsarafarajeanantoinnet@gmail.com>
2025-04-11 11:36:25 +0300

    ref:Add translation fonctionality to New component

--- src/components/New.tsx
+++ src/components/New.tsx
@@ -1,25 +1,26 @@
 import React from "react";
-
-const New:React.FC = () => {
+import { useTranslation } from "react-i18next";
+const New: React.FC = () => {
+    const { t } = useTranslation()
     return (
         <>
             <div className="flex flex-wrap gap-4 md:gap-6 lg:gap-10 p-3 md:p-4 justify-center mb-5 mt-3 w-full">
                 <div className="relative w-full sm:w-64 md:w-72 lg:w-52">
-                    <span className="absolute -right-2 -top-2 badge badge-primary z-10">New</span>
+                    <span className="absolute -right-2 -top-2 badge badge-primary z-10">{t("new")}</span>
                     <div className="bg-base-300 grid h-64 sm:h-64 md:h-72 lg:h-52 place-items-center rounded-lg w-full">
-                        content
+                        {t("newContent")}
                     </div>
                 </div>
                 <div className="relative w-full sm:w-64 md:w-72 lg:w-52">
-                    <span className="absolute -right-2 -top-2 badge badge-primary z-10">New</span>
+                    <span className="absolute -right-2 -top-2 badge badge-primary z-10">{t("new")}</span>
                     <div className="bg-base-300 grid h-64 sm:h-64 md:h-72 lg:h-52 place-items-center rounded-lg w-full">
-                        content
+                        {t("newContent")}
                     </div>
                 </div>
                 <div className="relative w-full sm:w-64 md:w-72 lg:w-52">
-                    <span className="absolute -right-2 -top-2 badge badge-primary z-10">New</span>
+                    <span className="absolute -right-2 -top-2 badge badge-primary z-10">{t("new")}</span>
                     <div className="bg-base-300 grid h-64 sm:h-64 md:h-72 lg:h-52 place-items-center rounded-lg w-full">
-                        content
+                        {t("newContent")}
                     </div>
                 </div>
             </div>
