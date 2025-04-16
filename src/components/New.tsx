051ddd4edb1a3b6e47f79dc5ed11eea4472f9d2c
BranGitfox <vixfgit@gmail.com>
2025-04-16 15:05:47 +0300

    feat: add img src

--- src/components/New.tsx
+++ src/components/New.tsx
@@ -8,19 +8,21 @@ const New: React.FC = () => {
                 <div className="relative w-full sm:w-64 md:w-72 lg:w-52">
                     <span className="absolute -right-2 -top-2 badge badge-primary z-10">{t("new")}</span>
                     <div className="bg-base-300 grid h-64 sm:h-64 md:h-72 lg:h-52 place-items-center rounded-lg w-full">
-                        {t("newContent")}
+                        <img src={'/new3.png'} alt='new3_img'/>
+
                     </div>
                 </div>
                 <div className="relative w-full sm:w-64 md:w-72 lg:w-52">
                     <span className="absolute -right-2 -top-2 badge badge-primary z-10">{t("new")}</span>
                     <div className="bg-base-300 grid h-64 sm:h-64 md:h-72 lg:h-52 place-items-center rounded-lg w-full">
-                        {t("newContent")}
+                        <img src={'/new1.png'} alt='new1_img'/>
                     </div>
                 </div>
                 <div className="relative w-full sm:w-64 md:w-72 lg:w-52">
                     <span className="absolute -right-2 -top-2 badge badge-primary z-10">{t("new")}</span>
                     <div className="bg-base-300 grid h-64 sm:h-64 md:h-72 lg:h-52 place-items-center rounded-lg w-full">
-                        {t("newContent")}
+                        <img src={'/new2.png'} alt='new2_img'/>
+
                     </div>
                 </div>
             </div>
