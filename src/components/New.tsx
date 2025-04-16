ee36db22bc282d0f6c07481477dc3d4f2319c6e7
BranGitfox <vixfgit@gmail.com>
2025-04-16 15:17:38 +0300

    style: avoid full img

--- src/components/New.tsx
+++ src/components/New.tsx
@@ -8,20 +8,20 @@ const New: React.FC = () => {
                 <div className="relative w-full sm:w-64 md:w-72 lg:w-52">
                     <span className="absolute -right-2 -top-2 badge badge-primary z-10">{t("new")}</span>
                     <div className="bg-base-300 grid h-64 sm:h-64 md:h-72 lg:h-52 place-items-center rounded-lg w-full">
-                        <img src={'/new3.png'} alt='new3_img' className='w-full h-full'/>
+                        <img src={'/new3.png'} alt='new3_img' />
 
                     </div>
                 </div>
                 <div className="relative w-full sm:w-64 md:w-72 lg:w-52">
                     <span className="absolute -right-2 -top-2 badge badge-primary z-10">{t("new")}</span>
                     <div className="bg-base-300 grid h-64 sm:h-64 md:h-72 lg:h-52 place-items-center rounded-lg w-full">
-                        <img src={'/new1.png'} alt='new1_img' className='w-full h-full'/>
+                        <img src={'/new1.png'} alt='new1_img' />
                     </div>
                 </div>
                 <div className="relative w-full sm:w-64 md:w-72 lg:w-52">
                     <span className="absolute -right-2 -top-2 badge badge-primary z-10">{t("new")}</span>
                     <div className="bg-base-300 grid h-64 sm:h-64 md:h-72 lg:h-52 place-items-center rounded-lg w-full">
-                        <img src={'/new2.png'} alt='new2_img' className='w-full h-full'/>
+                        <img src={'/new2.png'} alt='new2_img' />
 
                     </div>
                 </div>
